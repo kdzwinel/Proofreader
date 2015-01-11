57e578b63fc01a4cd8dc78c910843b6f130956f3
Konrad Dzwinel <kdzwinel@gmail.com>
2015-01-12 00:28:00 +0100

    Moving dictionary configuration out of the Proofreader module

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,6 +2,7 @@
 
 //Helpers
 var fs = require('fs');
+var path = require('path');
 var request = require('request');
 var program = require('commander');
 var mime = require('mime');
@@ -17,11 +18,35 @@ program
   .option('-c, --config-file', 'Path to a configuration file')
   .parse(process.argv);
 
+//if custom config file was provided
 if(program.configFile) {
   config = require(program.configFile);
 }
 
-var proofreader = new Proofreader(config);
+//configuration validation
+if(!config) {
+  throw new Error('Configuration object missing.');
+} else if(!config.dictionaries['build-in'] || !config.dictionaries['build-in'].length) {
+  throw new Error('At least one build-in dictionary has to be set.');
+} else if(!config.selectors || !config.selectors.whitelist) {
+  throw new Error('Whitelist has to be set.');
+}
+
+var proofreader = new Proofreader();
+
+proofreader.setWhitelist(config.selectors.whitelist);
+proofreader.setBlacklist(config.selectors.blacklist);
+
+config.dictionaries['build-in'].forEach(function(dictName) {
+  proofreader.addDictionary(path.join(__dirname, '../dictionaries/' + dictName + '.dic'),
+    path.join(__dirname, '../dictionaries/' + dictName + '.aff'));
+});
+
+if(config.dictionaries['custom']) {
+  config.dictionaries['custom'].forEach(function(dictPath) {
+    proofreader.addDictionary(dictPath);
+  });
+}
 
 function toHTML(path, content) {
   var mimeType = mime.lookup(path);
@@ -49,14 +74,10 @@ if(program.url) {
   resultPromise = proofreader.proofread(toHTML(program.file, content));
 } else if(program.fileList) {
   var listOfFiles = fs.readFileSync(program.fileList).toString().split("\n");
-  var promises = [];
-
-  listOfFiles.forEach(function(filePath) {
+  var promises = listOfFiles.map(function(filePath) {
     if(filePath) {
       var content = fs.readFileSync(filePath).toString();
-      var promise = proofreader.proofread(toHTML(filePath, content));
-
-      promises.push(promise);
+      return proofreader.proofread(toHTML(filePath, content));
     }
   });
 
