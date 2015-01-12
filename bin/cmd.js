1ccec6dca771bc345b31b804bd3e421ac4ecb8a0
Konrad Dzwinel <kdzwinel@gmail.com>
2015-01-12 01:06:33 +0100

    Moving printing result out of the Proofreader module

--- bin/cmd.js
+++ bin/cmd.js
@@ -8,6 +8,7 @@ var program = require('commander');
 var mime = require('mime');
 var marked = require('marked');
 var Promise = require('promise');
+var clc = require('cli-color');
 var config = require('../settings.json');
 var Proofreader = require('../lib/proofreader.js');
 
@@ -19,16 +20,16 @@ program
   .parse(process.argv);
 
 //if custom config file was provided
-if(program.configFile) {
+if (program.configFile) {
   config = require(program.configFile);
 }
 
 //configuration validation
-if(!config) {
+if (!config) {
   throw new Error('Configuration object missing.');
-} else if(!config.dictionaries['build-in'] || !config.dictionaries['build-in'].length) {
+} else if (!config.dictionaries['build-in'] || !config.dictionaries['build-in'].length) {
   throw new Error('At least one build-in dictionary has to be set.');
-} else if(!config.selectors || !config.selectors.whitelist) {
+} else if (!config.selectors || !config.selectors.whitelist) {
   throw new Error('Whitelist has to be set.');
 }
 
@@ -37,13 +38,13 @@ var proofreader = new Proofreader();
 proofreader.setWhitelist(config.selectors.whitelist);
 proofreader.setBlacklist(config.selectors.blacklist);
 
-config.dictionaries['build-in'].forEach(function(dictName) {
+config.dictionaries['build-in'].forEach(function (dictName) {
   proofreader.addDictionary(path.join(__dirname, '../dictionaries/' + dictName + '.dic'),
     path.join(__dirname, '../dictionaries/' + dictName + '.aff'));
 });
 
-if(config.dictionaries['custom']) {
-  config.dictionaries['custom'].forEach(function(dictPath) {
+if (config.dictionaries['custom']) {
+  config.dictionaries['custom'].forEach(function (dictPath) {
     proofreader.addDictionary(dictPath);
   });
 }
@@ -51,41 +52,87 @@ if(config.dictionaries['custom']) {
 function toHTML(path, content) {
   var mimeType = mime.lookup(path);
 
-  if(mimeType === 'text/x-markdown') {
+  if (mimeType === 'text/x-markdown') {
     return marked(content);
   }
 
   return content;
 }
 
-var resultPromise = null;
+function printResults(title, results) {
+  console.log('### Results for ' + title + ' ###');
+  console.log();
 
-if(program.url) {
+  results.forEach(function (result) {
+    var writeGood = result.suggestions.writeGood;
+    var spelling = result.suggestions.spelling;
+
+    //Printing output
+    if (writeGood.length || spelling.length) {
+      console.log(clc.red(result.text));
+
+      writeGood.forEach(function (item) {
+        console.log(clc.blue.bold(' - ' + item.reason));
+      });
+
+      spelling.forEach(function (item) {
+        console.log(clc.magenta.bold(' - "' + item.word + '" -> ' + item.suggestions));
+      });
+
+      console.log();
+    }
+  });
+}
+
+if (program.url) {
   request({uri: program.url}, function (err, response, body) {
     if (err) {
       throw err;
     }
 
-    resultPromise = proofreader.proofread(toHTML(program.url, body));
+    proofreader.proofread(toHTML(program.url, body))
+      .then(function (results) {
+        printResults(program.url, results);
+
+        if(results.length) {
+          process.exit(1);
+        }
+      });
   });
-} else if(program.file) {
+} else if (program.file) {
   var content = fs.readFileSync(program.file).toString();
 
-  resultPromise = proofreader.proofread(toHTML(program.file, content));
-} else if(program.fileList) {
+  proofreader.proofread(toHTML(program.file, content))
+    .then(function (results) {
+      printResults(program.file, results);
+
+      if(results.length) {
+        process.exit(1);
+      }
+    });
+} else if (program.fileList) {
   var listOfFiles = fs.readFileSync(program.fileList).toString().split("\n");
-  var promises = listOfFiles.map(function(filePath) {
-    if(filePath) {
+  var promises = listOfFiles.map(function (filePath) {
+    if (filePath) {
       var content = fs.readFileSync(filePath).toString();
-      return proofreader.proofread(toHTML(filePath, content));
+      return proofreader.proofread(toHTML(filePath, content))
+        .then(function (result) {
+          printResults(filePath, result);
+          return result;
+        });
     }
   });
 
-  resultPromise = Promise.all(promises);
-}
+  Promise.all(promises).then(function(files) {
+    var clean = true;
+    files.forEach(function(paragraphs) {
+      if(paragraphs.length > 0) {
+        clean = false;
+      }
+    });
 
-if(resultPromise) {
-  resultPromise.then(null, function() {
-    process.exit(1);
+    if(!clean) {
+      process.exit(1);
+    }
   })
 }
